10b2da9b74db25158003a77ded0e13d8157aef35
13558642643 <55170531+13558642643@users.noreply.github.com>
2020-10-23 11:05:28 +0800

    Update MyThreadPoolExecutor.java

--- src/main/java/cn/zxw/ztestboot/threadPoolExecutor/MyThreadPoolExecutor.java
+++ src/main/java/cn/zxw/ztestboot/threadPoolExecutor/MyThreadPoolExecutor.java
@@ -1,11 +1,17 @@
 package cn.zxw.ztestboot.threadPoolExecutor;
 
+import javassist.ClassPool;
+import javassist.bytecode.ConstPool;
+import javassist.bytecode.analysis.Executor;
+
 /**
  * @author : ZXW
  * @version : 1.0
  * @date : 2020-10-16 14:08
  * @Description : 手写线程池
  */
-public class MyThreadPoolExecutor {
-
+public class MyThreadPoolExecutor extends Executor {
+    public MyThreadPoolExecutor(ClassPool classPool, ConstPool constPool) {
+        super(classPool, constPool);
+    }
 }
